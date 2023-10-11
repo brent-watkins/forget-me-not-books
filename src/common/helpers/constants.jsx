bb6bc35f44490738b0b74b54c767ff2a94b6369f
Brent Watkins <brent.n.watkins@gmail.com>
2023-10-11 12:58:27 -0600

    feature: Create a public facing navigation bar

---
+++ src/common/helpers/constants.jsx
@@ -0,0 +1,3 @@
+export const SHOW_ON_MOBILE_ONLY_SX = { display: { xs: "block", sm: "none" } };
+export const SHOW_ON_TABLET_ONLY_SX = { display: { xs: "none", sm: "block", md: "none" } };
+export const SHOW_ON_DESKTOP_ONLY_SX = { display: { xs: "none", md: "block" } };
