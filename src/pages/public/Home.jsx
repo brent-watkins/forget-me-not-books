6f81f7b7db700c19dfa1a6a59fd44e929e992847
Brent Watkins <brent.n.watkins@gmail.com>
2024-04-04 08:16:54 -0600

    feature: Create a basic public facing home page

--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React from "react";
+
+import { Button, Grid, Typography } from "@mui/material";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <Grid alignItems="center" container direction="column" spacing={8}>
+      <Grid item>
+        <Typography align="center" component="h1" variant="h3">
+          Welcome to Forget Me Not Books!
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography align="center" component="p" variant="h5">
+          Save your memories in beautiful, easily customizable layouts that you
+          can share with family and friends!
+        </Typography>
+      </Grid>
+      <Grid
+        alignItems="center"
+        container
+        item
+        justifyContent="center"
+        spacing={2}
+      >
+        <Grid item>
+          <Button href="/" size="large" variant="contained">
+            Get Started
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button
+            color="secondary"
+            href="/about"
+            size="large"
+            variant="contained"
+          >
+            Learn More
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
